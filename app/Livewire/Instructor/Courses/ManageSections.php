5c722c6cf01d2f75cc5b65718fa6f849fafe7c6a
CortezWeb <hamiltoncortezweb@gmail.com>
2025-07-23 19:15:54 -0400

    Crear una nueva Seccion

--- app/Livewire/Instructor/Courses/ManageSections.php
+++ app/Livewire/Instructor/Courses/ManageSections.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Instructor\Courses;
 
+use App\Models\Section;
 use Livewire\Component;
 
 class ManageSections extends Component
@@ -10,6 +11,18 @@ class ManageSections extends Component
     public $course;
 
     public $name;
+
+    public $sections;
+
+    public function mount($course)
+    {
+       $this->sections = Section::where('course_id', $this->course->id)
+       ->orderBy('position', 'asc' )
+       ->get();
+    }
+
+
+
 
     public function store()
     {
--- resources/views/livewire/instructor/courses/manage-sections.blade.php
+++ resources/views/livewire/instructor/courses/manage-sections.blade.php
@@ -1,5 +1,40 @@
 <div>
+    {{--Listar Section--}}
 
+    <ul class="mb-6 space-y-6">
+        @foreach ($sections as $section)
+            <li>
+                <div class="bg-gray-100 rounded-lg shadow-lg px-6 py-4 mb-4">
+                    <div class="md:flex md:items-center">
+                    <h1 class="md:flex-1 truncate">
+                        Seccion{{$section->position}}:
+                        <br class="md:hidden">
+                        <span class="font-semibold">
+                            {{ $section->name }}
+                        </span>
+                    </h1>
+
+                    <div class="space-x-3 md:shrink-0 md:ml-4">
+                        <button>
+                            <i class="fas fa-edit hover:text-indigo-600"></i>
+                        </button>
+
+                        <button>
+                            <i class="fas fa-trash-alt hover:text-red-600"></i>
+                        </button>
+                    </div>
+
+                </div>
+
+
+            </div>
+            </li>
+
+
+        @endforeach
+    </ul>
+
+    {{--Crear Section --}}
     <form wire:submit="store">
         <div class="bg-gray-100 rounded-lg shadow-lg p-6">
             <x-label>
