20bf15fc548e9d8b5df7ee6174b3cbc1752818de
CortezWeb <hamiltoncortezweb@gmail.com>
2025-07-31 12:06:40 -0400

    Crear modelo y migracion Lesson

---
+++ app/Models/Lesson.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use App\Observers\LessonObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Model;
+
+
+#[ObservedBy([LessonObserver::class])]
+
+class Lesson extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'platform',
+        'video_path',
+        'video_original_name',
+        'image_path',
+        'description',
+        'duration',
+        'position',
+        'is_published',
+        'is_preview',
+        'is_processed',
+        'section_id'
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+        'is_preview' => 'boolean',
+        'is_processed' => 'boolean',
+    ];
+    //relacion uno a muchos inversa
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
+
+    //relacion uno a muchos
+    public function lessons()
+    {
+        return $this->hasMany(Lesson::class);
+    }
+}
