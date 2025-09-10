c2954444d70a1e9d722c4d503592c53c9447e71b
CortezWeb <hamiltoncortezweb@gmail.com>
2025-09-10 10:59:42 -0400

    Welcome Frotend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,67 @@
 <x-app-layout>
+
+    <figure class="mb-20" >
+        <img class="w-full aspect-[1/1]  md:aspect-[3/1] object-cover object-center" src="{{asset('img/welcome/banner1.jpg')}}" alt="">
+    </figure>
+
+    <section>
+        <x-container>
+        <h1 class="text-center text-3xl md:text-4xl font-bold mb-6">
+            Contenido
+        </h1>
+        <ul class="grid grid-cols-4 gap-6">
+            <li>
+                <a href="">
+                    <img class="aspect-video object-cover object-center rounded-lg" src="{{asset('img/welcome/drones.jpg')}}" alt="">
+                </a>
+                <h1 class="text-xl text-center font-semibold mt-4 mb-2">
+                    <a href="">
+                        Curso online
+                    </a>
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga natus beatae itaque blanditiis illo expedita possimus. Dolorum hic veniam iusto. Perspiciatis eaque cumque explicabo suscipit deserunt odio voluptatem expedita quasi.</p>
+            </li>
+            <li>
+                <a href="">
+                    <img class="aspect-video object-cover object-center rounded-lg" src="{{asset('img/welcome/hardware.jpg')}}" alt="">
+                </a>
+                <h1 class="text-xl text-center font-semibold mt-4 mb-2">
+                    <a href="">
+                        Curso online
+                    </a>
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga natus beatae itaque blanditiis illo expedita possimus. Dolorum hic veniam iusto. Perspiciatis eaque cumque explicabo suscipit deserunt odio voluptatem expedita quasi.</p>
+            </li>
+            <li>
+                <a href="">
+                    <img class="aspect-video object-cover object-center rounded-lg" src="{{asset('img/welcome/mobile.jpg')}}" alt="">
+                </a>
+                <h1 class="text-xl text-center font-semibold mt-4 mb-2">
+                    <a href="">
+                        Curso online
+                    </a>
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga natus beatae itaque blanditiis illo expedita possimus. Dolorum hic veniam iusto. Perspiciatis eaque cumque explicabo suscipit deserunt odio voluptatem expedita quasi.</p>
+            </li>
+            <li>
+                <a href="">
+                    <img class="aspect-video object-cover object-center rounded-lg" src="{{asset('img/welcome/asesorias.jpg')}}" alt="">
+                </a>
+                <h1 class="text-xl text-center font-semibold mt-4 mb-2">
+                    <a href="">
+                        Curso online
+                    </a>
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga natus beatae itaque blanditiis illo expedita possimus. Dolorum hic veniam iusto. Perspiciatis eaque cumque explicabo suscipit deserunt odio voluptatem expedita quasi.</p>
+            </li>
+
+
+        </ul>
+
+
+        </h1>
+    </x-container>
+    </section>
+
+
 </x-app-layout>
